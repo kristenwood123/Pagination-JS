3a0ae749505ec67f327978eaf1087fdab258b0be
Kristen <kristenwood123@gmail.com>
2021-12-07 21:40:35 -0800

    active button

--- displayButtons.js
+++ displayButtons.js
@@ -1,5 +1,8 @@
 const displayButtons = (container, pages, activeIndex) => {
-
+  let btns = pages.map((_, pageIndex) => {
+    return `<button class='page-btn ${activeIndex === pageIndex ? 'active-btn': 'null'}>${pageIndex + 1}</button>`
+  })
+  container.innerHTML = btns.join('')
 }
 
 export default displayButtons
