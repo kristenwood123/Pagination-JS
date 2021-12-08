f801b765aab15dd92116f37dbd03ba436136b4ad
Kristen <kristenwood123@gmail.com>
2021-12-07 20:00:32 -0800

    displayed followers

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 import fetchFollowers from './fetchFollowers.js'
-import displayFollowers from './displayFollowers.js'
-import paginate from './paginate.js'
-import displayButtons from './displayButtons.js'
+// import displayFollowers from './displayFollowers.js'
+// import paginate from './paginate.js'
+// import displayButtons from './displayButtons.js'
+
+const title = document.querySelector('.section-title h1')
+
+const init = async () => {
+  const followers = await fetchFollowers()
+  title.textContent = 'pagination'
+}
+
+window.addEventListener('load', init)
